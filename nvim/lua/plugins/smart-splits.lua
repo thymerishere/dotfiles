0206b4065a5dfd22ed036061ef5109108538b3b2
Thomas Bos <thymer.bos217@gmail.com>
2026-05-19 10:19:36 +0200

    Fix smart splits zoom nav

--- nvim/lua/plugins/smart-splits.lua
+++ nvim/lua/plugins/smart-splits.lua
@@ -1,6 +1,11 @@
 return {
   'mrjones2014/smart-splits.nvim',
   config = function()
+    require('smart-splits').setup({
+      multiplexer_integration = 'tmux',
+      disable_multiplexer_nav_when_zoomed = false,
+    })
+
     -- recommended mappings
     -- resizing splits
     -- these keymaps will also accept a range,
@@ -9,12 +14,14 @@ return {
     -- vim.keymap.set('n', '<A-j>', require('smart-splits').resize_down)
     -- vim.keymap.set('n', '<A-k>', require('smart-splits').resize_up)
     -- vim.keymap.set('n', '<A-l>', require('smart-splits').resize_right)
+
     -- moving between splits
     vim.keymap.set('n', '<C-h>', require('smart-splits').move_cursor_left)
     vim.keymap.set('n', '<C-j>', require('smart-splits').move_cursor_down)
     vim.keymap.set('n', '<C-k>', require('smart-splits').move_cursor_up)
     vim.keymap.set('n', '<C-l>', require('smart-splits').move_cursor_right)
     vim.keymap.set('n', '<C-\\>', require('smart-splits').move_cursor_previous)
+
     -- swapping buffers between windows
     vim.keymap.set('n', '<leader><leader>h', require('smart-splits').swap_buf_left)
     vim.keymap.set('n', '<leader><leader>j', require('smart-splits').swap_buf_down)
